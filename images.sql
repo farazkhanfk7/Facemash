8365fdcdecb8ffa3929cc5e26e3a1e33fc0736b7
Faraz Khan <47111858+farazkhanfk7@users.noreply.github.com>
2019-11-19 19:01:01 +0530

    Update images.sql

--- images.sql
+++ images.sql
@@ -23,7 +23,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `images`
+-- Table structure foor table `images`
 --
 
 CREATE TABLE `images` (
